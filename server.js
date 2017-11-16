9fe5973df812ec30132029c2fd01705bec457dfe
Valerio Volpe <vvolpe97@gmail.com>
2017-11-16 17:09:35 +0100

    Commit for start

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const express = require("express"),
       path = require("path");
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.use('/', express.static(__dirname));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
